af1ff695e78a762063516e17c2e58c3c3286d4cc
Eetu Laine <eetu.laine@aalto.fi>
2020-05-12 17:43:09 +0300

    Added some ideas and more structure

--- policies/RuleAgent.py
+++ policies/RuleAgent.py
@@ -4,11 +4,24 @@ class Agents():
 
 	def __init__(self):
 		self.count = 4
+		self.t = 0
 
 
 	''' Proposing the following rule-based system: Seeking agents allocate between eachother targets to pursue based 
 	on distances to targets, and move towards them(because the agents are given the other agents' positions anyway). The hiders move away from the seekers in some 
 	weighted manner.'''
+
+	'''
+	Ideas: 
+
+		- keep a moving average of agent velocities/positions to check if we have hit a wall
+
+		- compute hiders actions based on wanting to get out of the line of sight of agents who see them, 
+		also try to avoid getting stuck in walls if agent is seen
+
+	'''
+
+	
 	def act(self, ob):
 		action_movement = np.random.randint(0, 10, (self.count, 3))
 		action_pull = np.random.randint(0, 1, 4)
@@ -37,3 +50,6 @@ class Agents():
 				pass
 		action = {'action_movement': action_movement, 'action_pull': action_pull, 'action_glueall': action_glueall}
 		return action
+
+	def clear_memory(self):
+		self.t = 0
